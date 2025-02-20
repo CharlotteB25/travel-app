5880f43ae8e118ad3c061e90524aa1c9a340074b
Charlotte Billiet <charbill@student.arteveldehs.be>
2025-02-20 16:38:06 +0100

    css bug in serttings

--- app/src/components/pages/settings/settings.ts
+++ app/src/components/pages/settings/settings.ts
@@ -1,4 +1,4 @@
-import { LitElement, html, css } from "lit";
+import { LitElement, html } from "lit";
 import { customElement, property } from "lit/decorators.js";
 import { defaultStyles } from "@components/style/styles";
 import { UserBody } from "@core/modules/user/User.types";
