e3be9069a3d65a347bdec111bdd3b6a2da3c6d47
Charlotte Billiet <charbill@student.arteveldehs.be>
2025-10-25 15:54:14 +0200

    vite-env

--- app/src/vite-env.d.ts
+++ app/src/vite-env.d.ts
@@ -5,6 +5,7 @@ interface ImportMetaEnv {
   readonly VITE_API_URL: string;
   readonly VITE_APP_TITLE: string;
   readonly VITE_WEATHER_API: string;
+  readonly VITE_RAPIDAPI_KEY: string;
   // Add other environment variables as needed
 }
 
